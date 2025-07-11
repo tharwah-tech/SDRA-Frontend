0b35256994f98487ccf4b2232f09f9e3984cd0e7
Ahmed Magdy Ahmed <Ahmed.magdi@tharwah.net>
2025-07-11 17:09:40 +0300

    add main page structure into agent view details

--- src/app/features/agents/presentation/pages/agent-view-page/agent-view-page.component.ts
+++ src/app/features/agents/presentation/pages/agent-view-page/agent-view-page.component.ts
@@ -6,13 +6,18 @@ import { AgentsFacade } from '../../facades/agents.facade';
 import { filter, takeUntil, tap } from 'rxjs';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { AgentEntity } from '../../../domain/entities/agent.entity';
+import { MainPageStructureComponent } from "../../../../../shared/components/main-page-structure/main-page-structure.component";
+import { RouteLink } from '../../../../../shared/components/page-navigation-routes/page-navigation-routes.component';
+import { SideNavTabs } from '../../../../../core/enums/side-nave-tabs.enum';
 @Component({
   selector: 'app-agent-view-page',
-  imports: [CommonModule, AgentCardComponent, MatProgressSpinnerModule],
+  imports: [CommonModule, AgentCardComponent, MatProgressSpinnerModule, MainPageStructureComponent],
   templateUrl: './agent-view-page.component.html',
   styleUrl: './agent-view-page.component.scss',
 })
 export class AgentViewPageComponent implements OnInit {
+  SideNavTabs = SideNavTabs;
+  lang = input.required<string>();
   agent$;
   selectedAgent: AgentEntity | null = null;
   loading$;
@@ -38,6 +43,12 @@ export class AgentViewPageComponent implements OnInit {
         }
       ))
       .subscribe();
+  }
+  CurrentPagePath(): RouteLink[]{
+    return [
+      { path: `/${this.lang()}/agents`, label: 'AI Agents' },
+      { path: `/${this.lang()}/agents/agent/${this.id()}`, label: this.selectedAgent?.name || 'Agent Details' }
+    ];
   }
 }
 
