d6ded92bb69f4a0fc55d94ae27ea969437931d92
Ahmed Magdy Ahmed <Ahmed.magdi@tharwah.net>
2025-07-14 16:05:38 +0300

    add error handle for login page

--- src/app/features/authentication/presentation/pages/login-page/login-page.component.ts
+++ src/app/features/authentication/presentation/pages/login-page/login-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, DestroyRef, input } from '@angular/core';
+import { Component, DestroyRef, input, OnInit } from '@angular/core';
 import { LoginFormComponent } from '../../components/login-form/login-form.component';
 import { Router } from '@angular/router';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
@@ -6,6 +6,11 @@ import { CommonModule } from '@angular/common';
 import { ToastrService } from 'ngx-toastr';
 import { LanguageService } from '../../../../../core/services/language.service';
 import { Store } from '@ngrx/store';
+import { filter, Observable, tap } from 'rxjs';
+import { ApiError } from '../../../../../core/models/api-error.model';
+import { selectAuthError } from '../../store/auth.selectors';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { showSnackbar } from '../../../../../shared/utils/show-snackbar-notification.util';
 
 @Component({
   selector: 'app-login-page',
@@ -13,8 +18,9 @@ import { Store } from '@ngrx/store';
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss',
 })
-export class LoginPageComponent {
+export class LoginPageComponent implements OnInit {
   lang = input.required<string>();
+  error$: Observable<ApiError | null>;
   constructor(
     public translateService: TranslateService,
     public languageService: LanguageService,
@@ -22,7 +28,20 @@ export class LoginPageComponent {
     private toastr: ToastrService,
     private router: Router,
     private store: Store
-  ) {}
+  ) {
+    this.error$ = this.store.select(selectAuthError);
+  }
+  ngOnInit() {
+    this.error$
+      .pipe(
+        takeUntilDestroyed(this.destroyRef),
+        filter((val) => !!val),
+        tap((error) => {
+          showSnackbar(this.toastr, { error, type: 'error' });
+        })
+      )
+      .subscribe();
+  }
   goToRegister() {
     this.router.navigate([`${this.lang()}/auth/register`]);
   }
