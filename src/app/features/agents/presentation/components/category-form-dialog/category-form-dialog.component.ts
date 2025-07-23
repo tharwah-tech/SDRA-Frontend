77cbf3aba31d89f1c34d82fe25e391ef045df25e
MohamedNabarawy <mohammed.gamal@tharwah.net>
2025-07-23 12:54:27 +0300

    new category component empty

---
+++ src/app/features/agents/presentation/components/category-form-dialog/category-form-dialog.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-category-form-dialog',
+  imports: [],
+  templateUrl: './category-form-dialog.component.html',
+  styleUrl: './category-form-dialog.component.scss'
+})
+export class CategoryFormDialogComponent {
+
+}
